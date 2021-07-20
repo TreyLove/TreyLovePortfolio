8412eb70c355df5b2be62187b388b96ff254e219
Trey Love <theendurinpupil@gmail.com>
2021-07-19 18:26:38 -0700

    added partially finished portfolio

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Projects from './components/Project'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from "./components/About"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div>
+
+
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div >
+      </Router>
+      <Footer />
+    </div>
+
+  );
+}
+
+
+
+
+export default App;
+
---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+
+
+export default function Header() {
+    return (
+        <div className='headerCont'>
+            <div className="header">
+                <h1>Trey Love</h1>
+            </div>
+
+            <ul className="navLinks">
+                <li>
+                    <Link to="/"> Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/about">About </Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects </Link>
+                </li>
+
+            </ul>
+
+        </div>
+    )
+}
